35ea19303170cecbf3cec850de10b642aae4a4ca
Flummox <avrorasim@mail.ru>
2026-02-08 20:54:57 +0300

    Restructured Docker Compose and DB init files

--- DNDhelper/Program.cs
+++ DNDhelper/Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+        app.Map("/" ,() => "Hello world");
 
-app.Run();
+        app.Run();        
+    }
+}
